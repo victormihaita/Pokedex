51d66737d4942a62806d5a311c780641997365fc
Victor Mihaita <43206741+victormihaita@users.noreply.github.com>
2021-10-13 13:52:34 +0300

    Initial Commit

---
+++ Shared/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Shared
+//
+//  Created by MacBook on 13.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
